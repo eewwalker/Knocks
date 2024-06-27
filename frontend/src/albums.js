c5acfcb3bc06646646a6acb149608171c94f1f34
Emily Walker <ewalker415@gmail.com>
2024-06-26 19:05:39 -0700

    temp data

---
+++ frontend/src/albums.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const albums = [
+  {
+    _id: '1',
+    name: 'MadVillian',
+    artist: 'Mf Doom and Madlib',
+    image: '/public/images/madvillain.jpeg',
+    genre: 'Hip Hop',
+    released: '2021',
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: '2',
+    name: 'Purple Rain',
+    artist: 'Prince And The Revolution',
+    image: '/public/images/prince.jpeg',
+    genre: 'Rock, Funk/Soul, Pop',
+    released: '1984',
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: '3',
+    name: 'Renaissance',
+    artist: 'Beyonce',
+    image: '/public/images/beyonce.jpeg',
+    genre: 'House, Dance-pop, Contemporary R&B, Ballroom, Hip-House',
+    released: '2022',
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: '4',
+    name: 'Ego Death',
+    artist: 'The Internet',
+    image: '/public/images/internet.jpeg',
+    genre: 'Hip Hop, Funk/Soul',
+    released: '2015',
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    _id: '5',
+    name: 'Motomami',
+    artist: 'Rosalia',
+    image: '/public/images/rosalia.jpeg',
+    genre: 'Electronic, Hip Hop, Latin, Pop',
+    released: '2022',
+    rating: 0,
+    numReviews: 0,
+  }
+]
+
+export default albums;
