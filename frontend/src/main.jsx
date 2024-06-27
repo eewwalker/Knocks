68b0792d74a0158e20f953b7e58b53107afffafa
Emily Walker <ewalker415@gmail.com>
2024-06-26 18:56:16 -0700

    frontend structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import HomeScreen from './screens/HomeScreen.jsx';
+import AlbumScreen from './screens/AlbumScreen.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {index: true, element: <HomeScreen/>},
+      {path: '/albums/:id', element: <AlbumScreen/>}
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
